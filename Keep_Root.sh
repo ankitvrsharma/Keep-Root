866da72ec36a58702eb0dedd8cbae143b8ba9cd1
Ankit Sharma <46074694+ankitvrsharma@users.noreply.github.com>
2024-05-03 22:19:35 +0530

    Update Keep_Root.sh
    Improve the code and add option to root using any magisk variant.

--- Keep_Root.sh
+++ Keep_Root.sh
@@ -1,157 +1,129 @@
 #!/bin/bash
-set -euo pipefail
 
+Y="\033[1;33m"
+G="\033[1;32m"
+R="\033[1;31m"
+NC="\033[0m"
 
-echo -e "\033[1;33mKeep Root\033[0m"
-# Set up environment 
-echo -e "\033[1;32mDownloading latest Magisk....\033[0m"
+MAGISK_REPO="topjohnwu/Magisk"
+APATCH_REPO="bmax121/APatch"
 
-mkdir tmp tmp1 && cd "$HOME/tmp1" && chmod 755 "$HOME/tmp1" || exit 1
+tmp_dir="$HOME/tmp"
+mkdir -p "$tmp_dir" "$tmp_dir/temp"
+chmod 755 "$tmp_dir/temp" || { echo -e "${R}Failed to set permissions for tmp directory.${NC}"; exit 1; }
 
-latest_release=$(curl -s https://api.github.com/repos/topjohnwu/Magisk/releases/latest | grep -oP '"tag_name": "\K(.*)(?=")')
-curl -L -o "magisk.zip" "https://github.com/topjohnwu/Magisk/releases/download/$latest_release/Magisk-$latest_release.apk"
-
-unzip -q ~/tmp1/magisk.zip || exit 1
-
-cd ~/tmp1/lib/arm64-v8a
-for f in *.so; do mv -- "$f" "${f#lib}"; done
-find -type f -name '*.so' | while read f; do mv "$f" "${f%.so}"; done
-
-cd ~/
-mv ~/tmp1/lib/arm64-v8a/* ~/tmp
-cp -r ~/tmp1/assets/* ~/tmp && rm -rf ~/tmp1
-
-sed -i '72,73s/false/true/g' ~/tmp/boot_patch.sh || exit 1
-
-echo "successful"
-wait
-
-  # Find boot image paths
+download() {
+  local repo=$1
+  local url="https://api.github.com/repos/$repo/releases/latest"
+  echo -e "${G}Downloading the latest release from $repo...${NC}"
   
-  sleep 1
-  su -c '
-    Y="\033[1;33m"
-    G="\033[1;32m"
-    R="\033[1;31m"
-    NC="\033[0m" # No Color
-    tmp_dir="/data/data/com.termux/files/home/tmp"
- 
- echo "${G}Extracting Boot...${NC}"
- 
- check_dir() {
-  if [ ! -d "$1" ]; then
-  echo -e "${R}Directory $1 does not exist. Aborting script...${NC}"
-  exit 1
+  local download_url=$(curl -s "$url" | grep -oP '"browser_download_url": "\K(.*?)(?=")')
+  if [ -z "$download_url" ]; then
+    echo -e "${R}Download URL not found.${NC}"
+    rm -rf $tmp_dir
+    exit 1
   fi
+  
+  curl -L -o "$tmp_dir/temp/latest.zip" "$download_url" || {
+    echo -e "${R}Failed to download the file.${NC}"
+    rm -rf $tmp_dir
+    exit 1
   }
-  check_dir "$tmp_dir"
+}
 
- check_success() {
+setup() {
+  echo -e "${G}Setting up environment...${NC}"
+  cd "$tmp_dir/temp" || exit 1
+  unzip -q latest.zip || { echo -e "${R}Failed to unzip the file.${NC}"; rm -rf $tmp_dir; exit 1; }
+  cd $tmp_dir/temp/lib/arm64-v8a
+  for f in *.so; do mv -- "$f" "${f#lib}"; done
+  find -type f -name '*.so' | while read f; do mv "$f" "${f%.so}"; done
+  cd $home
+  mv $tmp_dir/temp/lib/arm64-v8a/* $tmp_dir
+  cp -r $tmp_dir/temp/assets/* $tmp_dir
+  sed -i '72,73s/false/true/g' "$tmp_dir/boot_patch.sh"
+}
+
+echo -e "${Y}Enter the number corresponding to the root method you want to use:${NC}"
+echo -e "${G}1. Magisk${NC}"
+echo -e "${G}2. APatch${NC}"
+echo -e "${G}3. Other (Type GitHub Repo name i.e. magisk is topjohnwu/Magisk)${NC}"
+echo -e "${G}4. Skip for now${NC}"
+
+read -p "Selection: " user_choice
+
+case $user_choice in
+  1) download "$MAGISK_REPO";;
+  2) download "$APATCH_REPO";;
+  3) read -p "Enter the full GitHub repository path (e.g., 'username/repo'): " repo
+     download "$repo";;
+  4) echo -e "${Y}Skipping...${NC}"; rm -rf "$tmp_dir"; exit 0;;
+  *) echo -e "${R}Invalid option. Exiting...${NC}"; rm -rf $tmp_dir; exit 1;;
+esac
+
+setup
+
+su -c '
+# Define colors for echo
+Y="\033[1;33m"
+G="\033[1;32m"
+R="\033[1;31m"
+NC="\033[0m" # No Color
+tmp_dir="/data/data/com.termux/files/home/tmp"
+chmod 755 -R "$tmp_dir"
+
+cleanup() {
+  echo -e "${Y}Cleaning up temporary files...${NC}"
+  rm -rf "$tmp_dir"
+  echo -e "${G}Cleanup complete.${NC}"
+}
+
+check_error() {
   if [ $? -ne 0 ]; then
-  echo -e "${R}Previous command failed. Exiting...${NC}"
-  exit 1
+    echo -e "${R}An error occurred. Exiting...${NC}"
+    cleanup
+    exit 1
   fi
-  }
+}
 
-    boot_a=$(find /dev/block -type l -name boot_a -print | head -n 1)
-    boot_b=$(find /dev/block -type l -name boot_b -print | head -n 1)
-    active_slot=$(getprop ro.boot.slot_suffix) && check_success
-    echo "successful"
-    
- extract_inactive() {
-  if [[ "$active_slot" == "_a" ]]; then
-  dd if="$boot_b" of="$tmp_dir/boot.img" &>keep_root_error.txt
+root() {
+  local choice=$1
+  local active_slot=$(getprop ro.boot.slot_suffix)
+  local inactive_slot=$([[ "$active_slot" == "_a" ]] && echo "_b" || echo "_a")
+  local boot=$(find /dev/block -type l -name "boot$inactive_slot" -print | head -n 1)
+  
+  echo "Extracting boot image from $boot..."
+  if [ -n "$boot" ]; then
+    dd if="$boot" of="$tmp_dir/boot.img" || return 1
   else
-  dd if="$boot_a" of="$tmp_dir/boot.img" &>keep_root_error.txt
+    echo "Boot partition not found."
+    return 1
   fi
-  }
-            
- extract_active() {
-  if [[ "$active_slot" == "_a" ]]; then
-  dd if="$boot_a" of="$tmp_dir/boot.img" &>keep_root_error.txt
-  else
-  dd if="$boot_b" of="$tmp_dir/boot.img" &>keep_root_error.txt
-  fi
-  }
-  	  
- patch_magisk() {
-  chmod -R 755 $tmp_dir 
-  echo -e "${G}Patching boot.img with latest Magisk.....${NC}"
-  $tmp_dir/boot_patch.sh boot.img &>keep_root_error.txt || exit 1
-  wait
-  echo "successful"
-  }
-    
- verify_patch() {  
-  if [ ! -f "$tmp_dir/new-boot.img" ]; then
-  echo -e "${R}Error: Patched boot image file not found. Aborting.${NC}"
-  exit 1
-  fi
-  }
-     
- flash_active() {
-  if [[ "$active_slot" == "_a" ]]; then
-  dd if="$tmp_dir/new-boot.img" of="$boot_a" &>keep_root_error.txt
-  else
-  dd if="$tmp_dir/new-boot.img" of="$boot_b" &>keep_root_error.txt
-  fi	
-  }
-      
- flash_inactive() {
-  if [[ "$active_slot" == "_a" ]]; then
-  dd if="$tmp_dir/new-boot.img" of="$boot_b" &>keep_root_error.txt
-  else
-  dd if="$tmp_dir/new-boot.img" of="$boot_a" &>keep_root_error.txt
-  fi	
-  }
-   
- while true; do
-    echo -e "${Y}What do you want to do:${NC}"
-    echo "1. OTA update"
-    echo "2. Magisk update"
-    echo "3. Skip for now"
-    read -r user_choice
+  
+  echo -e "${G}Patching boot image...${NC}"
+  "$tmp_dir/boot_patch.sh" "$tmp_dir/boot.img" || return 1
+  
+  echo "${G}Flashing patched boot image to $boot...${NC}"
+  dd if="$tmp_dir/new-boot.img" of="$boot" || return 1
+}
 
- case "$user_choice" in
-  1)
-    extract_inactive && check_success
-    ls $tmp_dir &>keep_root_error.txt
-    patch_magisk && check_success
-    verify_patch
-    echo -e "${G}Flashing Magisk patched Boot....${NC}"
-    sleep 3
-    echo "successful"
-    flash_inactive && check_success
-    echo "${G}Clearing temporary files...${NC}"
-    sleep 3
-    echo "successful"
-    echo "${G}Magisk installation is successful. Reboot your phone to apply the changes.${NC}"
-    break
-    ;;
-  2)
-    extract_active && check_success
-    ls $tmp_dir > /dev/null 2>&1
-    patch_magisk && check_success
-    verify_patch
-    echo -e "${G}Flashing Magisk patched Boot...${NC}"
-    sleep 3
-    echo "successful"
-    flash_active && check_success
-    echo "${G}Clearing temporary files...${NC}"
-    sleep 3
-    echo "successful"
-    echo "${G}Magisk installation is successful. Reboot your phone to apply the changes.${NC}"
-    break
-    ;;
-  3)
-    echo -e "${G}Script execution skipped. You can flash it later.${NC}" && rm -rf $tmp_dir
-    exit 0
-    ;;
-  *)
-    echo -e "${R}Invalid choice. Please select 1, 2, or 3.${NC}"
-    ;;
- esac
+echo -e "${Y}Select what do you want to do:${NC}"
+options=("OTA Update" "Update root" "Skip for now")
+select opt in "${options[@]}"; do
+  case $REPLY in
+    1|2)
+      root $REPLY 
+      check_error
+      break
+      ;;
+    3)
+      echo -e "${Y}Rooting aborted. You can try later.${NC}"
+      cleanup
+      exit 0
+      ;;
+    *)
+      echo -e "${R}Invalid option. Try again.${NC}"
+      ;;
+  esac
 done
-rm -rf /data/data/com.termux/files/home/tmp
-rm -rf /data/data/com.termux/files/home/keep_root.sh
-  '
+'
